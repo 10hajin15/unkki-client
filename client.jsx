601d43f412608caa2619cfd74e11a969fa04d43f
hajin1015 <10hajin15@gmail.com>
2023-01-04 23:43:15 +0900

    client 수정

--- client.jsx
+++ client.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
-import Unkki from './Unkki';
+import Unkki from './unkki';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(<BrowserRouter><Unkki /></BrowserRouter>);
